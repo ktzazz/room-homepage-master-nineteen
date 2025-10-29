a6cc32ae57c32dfe461cc7cd2dc5f8fcc55c4f13
ktzazz <155502502+ktzazz@users.noreply.github.com>
2025-10-28 18:14:11 -0600

    js done

--- script.js
+++ script.js
@@ -20,3 +20,37 @@ const observer = new IntersectionObserver(handleIntersect, options);
 document.querySelectorAll(".fx-reveal").forEach(function (r) {
   observer.observe(r);
 });
+
+//gallery slider
+
+const slideGallery = document.querySelectorAll(".slide__gallery");
+const prevBtn = document.querySelector(".gallery__buttons .prev__btn");
+const nextBtn = document.querySelector(".gallery__buttons .next__btn");
+
+let currentImageIndex = 0;
+
+function updateGallery(newIndex) {
+  if (newIndex >= slideGallery.length) {
+    newIndex = 0;
+  } else if (newIndex < 0) {
+    newIndex = slideGallery.length - 1;
+  }
+
+  currentImageIndex = newIndex;
+
+  slideGallery.forEach((slide, index) => {
+    const isActive = index === currentImageIndex;
+
+    slide.classList.toggle("inactive", !isActive);
+  });
+}
+
+nextBtn.addEventListener("click", () => {
+  updateGallery(currentImageIndex + 1, "slideGallery");
+});
+
+prevBtn.addEventListener("click", () => {
+  updateGallery(currentImageIndex - 1, "slideGallery");
+});
+
+//end gallery slider
